0d14d2cefbedfc4326b64f5e54df1b2a4909f2bf
Shraddha <shraddha@blackcurrantapps.com>
2021-08-17 20:12:47 +0530

    Initial setup commit

---
+++ jest.config.js
@@ -0,0 +1,40 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // An array of regexp pattern strings used to skip coverage collection
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/tests/',
+    '/views/',
+    '/keys/',
+    '/public/',
+    '/models/',
+  ],
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // A path to a module which exports an async function that is triggered once before all test suites
+  globalSetup: './tests/setup.js',
+
+  // A path to a module which exports an async function that is triggered once after all test suites
+  globalTeardown: './tests/teardown.js',
+
+  // The paths to modules that run some code to configure or set up the testing environment before each test
+  setupFiles: [
+    './tests/globalmocks.js',
+  ],
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+}
